99ee35a19ae0054fe1884fe206665cd60cf3e63d
malerator <belik7337@gmail.com>
2023-02-20 21:09:25 +0300

    fix

--- script.js
+++ script.js
@@ -8,7 +8,14 @@
 
 //   return false;
 // }
-
+function resetHeight() {
+  // reset the body height to that of the inner browser
+  document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
 //////////////////////////////выделение навигации при загрузке страницы//////////////////////////////////////////
 window.onload = changeOnLoad;
 const tar = document.querySelectorAll(".panel__nav");
