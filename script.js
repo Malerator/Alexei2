a5ebf84effbb0377bd2dc5fb46f9772ff1b4289b
malerator <belik7337@gmail.com>
2023-02-24 23:25:33 +0300

    fix

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const appHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty("--app-height", "${window.innerHeight}px");
-};
-window.addEventListener("resize", appHeight);
-appHeight();
 ////////////////////////////////////
 document
   .querySelector("body")
