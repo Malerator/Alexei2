db25191eae6b701c974fe6ea226fc187032f51cb
malerator <belik7337@gmail.com>
2023-02-24 23:13:10 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const appHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--app-height", "${window.innerHeight}px");
+};
+window.addEventListener("resize", appHeight);
+appHeight();
+////////////////////////////////////
 document
   .querySelector("body")
   .addEventListener("wheel", preventScroll, { passive: false });
@@ -21,13 +28,13 @@ document.addEventListener(
   false
 );
 ////////////////////////////////////////////установка адаптивной высоты экрана////////////////////////////////////
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+// window.addEventListener("resize", () => {
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty("--vh", `${vh}px`);
+// });
 //////////////////////////////выделение навигации при загрузке страницы//////////////////////////////////////////
 window.onload = changeOnLoad;
 const tar = document.querySelectorAll(".panel__nav");
