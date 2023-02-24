0305d2c1fe95a7cdfd163a23acf41633257c742e
malerator <belik7337@gmail.com>
2023-02-24 15:28:02 +0300

    fix

--- script.js
+++ script.js
@@ -20,6 +20,41 @@ document.addEventListener(
   },
   false
 );
+// запрет поворота экрана
+// (A) LOCK SCREEN ORIENTATION
+function lock(orientation) {
+  // (A1) GO INTO FULL SCREEN FIRST
+  let de = document.documentElement;
+  if (de.requestFullscreen) {
+    de.requestFullscreen();
+  } else if (de.mozRequestFullScreen) {
+    de.mozRequestFullScreen();
+  } else if (de.webkitRequestFullscreen) {
+    de.webkitRequestFullscreen();
+  } else if (de.msRequestFullscreen) {
+    de.msRequestFullscreen();
+  }
+
+  // (A2) THEN LOCK ORIENTATION
+  screen.orientation.lock(orientation);
+}
+
+// (B) UNLOCK SCREEN ORIENTATION
+function unlock() {
+  // (B1) UNLOCK FIRST
+  screen.orientation.unlock();
+
+  // (B2) THEN EXIT FULL SCREEN
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Сначала получаем высоту окна просмотра
 // и умножаем ее на 1%
