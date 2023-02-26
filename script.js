9cecc4cd3c4c885f0fac040376563bbcb653eb35
malerator <belik7337@gmail.com>
2023-02-26 13:56:57 +0300

    fix

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener(
   false
 );
 ////////////////////////////////////////////установка адаптивной высоты экрана////////////////////////////////////
-let vh = window.innerHeight * 0.01;
+let vh = window.outerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 window.addEventListener("resize", () => {
